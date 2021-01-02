c435c0fa48a59c1a7355dd23459c44412463d600
colinflahive <cflahive@hotmail.com>
2021-01-02 18:56:43 +0000

    css added to experience.html and coordinates added to maps.js for master map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,6 @@
     function initMap() {
         let map = new google.maps.Map(document.getElementById("maps"), {
-            zoom: 3,
+            zoom: 6,
                 center: {
                     lat: 46.619261,
                     lng: -33.134766
@@ -10,9 +10,11 @@
             var labels = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
             
             var locations = [
-                { lat: 43.217, lng: 2.348 },
-                { lat: 41.084045, lng: -73.874245 },
-                { lat: 40.754932, lng: -73.984016 }
+                { lat: 43.206, lng: 2.364 },
+                { lat: 43.3370, lng: 2.3780 },
+                { lat: 42.80361, lng: 2.29944 },
+                { lat: 42.5214, lng: 2.3326 },
+                { lat: 42.8783, lng: 2.5056 }
             ];
             
             var markers = locations.map(function(location, i) {

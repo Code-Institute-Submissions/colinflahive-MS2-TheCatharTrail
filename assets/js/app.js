f0a074b6e8cba9e1d7ff6a3d037398816bf5a6ac
colinflahive <cflahive@hotmail.com>
2021-01-03 19:11:27 +0000

    updates made to index.html for testimonials and css media queries added

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-const navbarToggler = document.getElementsByClassName('navbar-toggler')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-navbarToggler.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})

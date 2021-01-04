73ea620e64f5f54d40aa108daa50473f9f7f8532
colinflahive <cflahive@hotmail.com>
2021-01-04 20:20:25 +0000

    js updated to include 'active' menu underline on each page

---
+++ assets/js/nav.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    for (var i = 0; i < document.links.length; i++) {
+    if (document.links[i].href == document.URL) {
+        document.links[i].className = "active";
+    }
+    }
+});

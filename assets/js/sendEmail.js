cf0901d60caf7959badb92e386f3920bbfffe6f3
colinflahive <cflahive@hotmail.com>
2021-03-01 07:39:50 +0000

    contact.html inserted radio buttons

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -13,6 +13,7 @@ function sendMail(contactForm) {
         },
         function(error) {
             console.log("FAILED", error);
+            
         });
         
         contactForm.reset();
